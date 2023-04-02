7b3aabda460e7bb9a32ccb79741be06be06180a7
EaglePB2 <eagleforceleader@hotmail.com>
2023-04-02 09:49:11 +0800

    minor fixes

--- Projects/Youtube_Thumbnail_Grabber/js/index.js
+++ Projects/Youtube_Thumbnail_Grabber/js/index.js
@@ -1,14 +1,23 @@
+
+
 function appendImage() {
     const input = document.getElementById("youtube-id");
     const videoID = input.value.match(/(?:\/|%3D|v=)([\w-]{11})(?:[%#?&]|$)/)[1];
     const imageTypes = ['default', 'sddefault', 'mqdefault', 'hqdefault', 'maxresdefault'];
+    const imageTypealt = ['sd','mq','hq','maxres'];
     let images = '';
 
+    imageTypes.forEach((type) => {
+        images += `<img src="http://img.youtube.com/vi/${videoID}/${type}.jpg" alt="${videoID}/${i}${type}">`;
+    });
+
     for (let i = 0; i <= 3; i++) {
-        imageTypes.forEach((type) => {
-            images += `<img src="http://img.youtube.com/vi/${videoID}/${i}.${type}.jpg" alt="${videoID}/${i}${type}">`;
+        imageTypealt.forEach((type) => {
+            images += `<img src="http://img.youtube.com/vi/${videoID}/${i}${type}.jpg" alt="${videoID}/${i}${type}">`;
         });
     }
+
+
 
     const imgContainer = document.getElementById("img");
     imgContainer.innerHTML = `<h1>影片缩略图截取结果：</h1>${images}`;
