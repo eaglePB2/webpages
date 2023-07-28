0c2ebe4e228c3cd6ea2cc982bd0179da2f80bb25
EaglePB2 <46810904+eaglePB2@users.noreply.github.com>
2023-07-28 17:49:18 +0800

    Moved to new repo

--- Projects/m3u8_Generator/execute.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#btn").on("click", function() {
-      $("#video").html("<source src='"+ $("#url").val() +"' type='application/x-mpegURL'>");
-      var ply = videojs("video");
-      ply.play();
-    });
-  });
